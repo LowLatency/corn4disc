61324cdbd7e625ca84506c35653fc3645b77713b
Tim <timiz0rpls@outlook.com>
2018-07-17 01:41:58 +0000

    Close the client when the window closes

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const { app, BrowserWindow } = require('electron');
 const Discord = require('discord.js');
 const { token } = require('./secrets.js');
 
-function createWindow()
-{
+function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    let win = new BrowserWindow({width: 800, height: 600})
     
     // and load the index.html of the app.
     win.loadFile('index.html')
+    
+    return win;
 };
 
 function main()
 {
-    createWindow();
-    
+    let win = createWindow();
     const client = new Discord.Client();
-    client.on('ready', () =>
-    {
+    
+    client.on('ready', () => {
       console.log(`Logged in as ${client.user.tag}!`);
     });
+    
+    client.login(token);
     
     client.on('message', msg => {
       if (msg.content === 'ping') {
@@ -27,8 +29,9 @@ function main()
       }
     });
     
-    
-    client.login(token);
+    win.on('closed', () => {
+        client.destroy();
+    });
 }
 
 app.on('ready', main);
